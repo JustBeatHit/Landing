4006c75528829198b2466e05a6b560c7c2a7b6e1
Chevallier Melvin <mchevallier@aubay.com>
2024-09-12 14:23:32 +0200

    update style and pricing style

--- src/components/Pricing.vue
+++ src/components/Pricing.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import ButtonInscription from './ButtonInscription.vue';
-import { FontawesomeObject } from '@fortawesome/fontawesome-svg-core';
-import { faHeart, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
 </script>
 <template>
     <div class="section-pricing">
@@ -11,42 +13,111 @@ import { faHeart, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
             <ul>
                 <li><h4>Gratuit</h4></li>
                 <li class="desc">Jouez aussi longtemps que vous le souhaitez</li>
-                <li><img src="../assets/img/icon/true.svg"> Musiques libre de droits</li>
-                <li><img src="../assets/img/icon/true.svg"> 3 jeux</li>
-                <li><img src="../assets/img/icon/true.svg"> 1 défi journalier</li>
-                <li><img src="../assets/img/icon/true.svg"> Mode multi-joueur</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" style="background-color:;"/> Musiques libre de droits</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> 3 jeux</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> 1 défi journalier</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Mode multi-joueur</li>
             </ul>
-            <ButtonInscription text="S'inscrire"/>
+            <ButtonInscription text="S'inscrire" id="btn-bg-secondary"></ButtonInscription>
           </div>
           <div class="pricing">
             <ul>
                 <li><h4>Journalier</h4></li>
                 <li class="desc">5€ / MOIS</li>
-                <li><img src="../assets/img/icon/true.svg"> Offre gratuite</li>
-                <li><img src="../assets/img/icon/true.svg"> Musiques du moment</li>
-                <li><img src="../assets/img/icon/true.svg"> Tous les défis journaliers</li>
-                <li><img src="../assets/img/icon/true.svg"> Mode de difficultés</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Offre gratuite</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Musiques du moment</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Tous les défis journaliers</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Mode de difficultés</li>
             </ul>
-            <ButtonInscription text="S'abonner"/>
+            <ButtonInscription text="S'abonner" id="btn-bg-secondary"></ButtonInscription>
           </div>
           <div class="pricing">
             <ul>
                 <li><h4>Premium</h4></li>
                 <li class="desc">10€ / MOIS</li>
-                <li>
-                    <FontawesomeObject :icon="faHeart" />
-                    <img src="../assets/img/icon/true.svg"> Offre journalier</li>
-                <li><img src="../assets/img/icon/true.svg"> Toutes les musiques</li>
-                <li><img src="../assets/img/icon/true.svg"> Tous les modes et défis</li>
-                <li><img src="../assets/img/icon/true.svg"> Ajout de fonctionnalités IA </li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Offre journalier</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Toutes les musiques</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Tous les modes et défis</li>
+                <li class="fa"><font-awesome-icon :icon="faCheck" /> Ajout de fonctionnalités IA </li>
             </ul>
-            <ButtonInscription text="S'abonner"/>
+            <ButtonInscription text="S'abonner" id="btn-bg-white"></ButtonInscription>
           </div>
         </div>
       </div>
   </template>
 
 
-<!-- create svg component -->
+<style lang="scss">
+@import '../styles/_variables.scss';
+
+    .section-pricing{
+    max-width: 1240px;
+    margin: 7rem auto 3rem auto;
+    
+    h2{
+        text-transform: uppercase;
+        font-size: 2rem;
+        text-align: center;
+    }
+}
+
+.container-pricings{
+    max-width: 1240px;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    gap: 1rem;
+
+    .pricing{
+        display: flex;
+        flex-direction: column;
+        gap: 1rem;
+        padding: 1rem;
+        border-radius: 10px;
+        color: white;
+        justify-content: space-between;
+
+        .fa{
+            padding: 0.25rem 0;
+        }
+        h4{
+            font-size: 2rem;
+            margin: 0;
+        }
+        .desc{
+            min-height: 4rem;
+            line-height: 1;
+            padding-bottom: 1rem;
+            font-size: 1.5rem;
+            font-weight: 700;
+        }
+
+    }
+
+    .pricing:nth-child(1) {
+    background-color: white;
+    color: black;
+    width: 30%;
+
+    .desc{
+        color: $secondary-color;
+    }
+    }
+    
+    .pricing:nth-child(2) {
+    background-color: white;
+    color: black;
+    width: 30%;
+    .desc{
+        color: $secondary-color;
+    }
+    }
+    
+    .pricing:nth-child(3) {
+    background-color: $secondary-color;
+    width: 40%
+    }
+}
+</style>
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-import './styles/main.scss'
 import App from './App.vue'
 
 createApp(App).mount('#app')
