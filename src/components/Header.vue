bcc5d3217941107a9da61d151801661d71d23de9
YesIDo49 <mehdialsidcheikh@gmail.com>
2024-09-13 09:39:54 +0200

    :lipstick: move to global style file and responsive pricing

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,59 +1,13 @@
 <script setup lang="ts">
 import Navbar from './NavBar.vue';
 import ButtonInscription from './ButtonInscription.vue';
-// import '../styles/_variables.scss';
 </script>
 <template>
   <header>
     <Navbar />
-    <div class="header">
+    <div class="header-jbh">
       <img src="../assets/img/Logo.svg" alt="Vue logo" />
       <ButtonInscription text="S'inscrire" class="btn-primary"/>
     </div>
   </header>
 </template>
-
-
-<style lang="scss">
-@import '../styles/_variables.scss';
-header{
-    color: white;
-    padding: 1rem;
-    margin: 0 auto;
-    max-width: 1240px;
-    display: flex;
-    flex-direction: column;
-    gap: 4rem;
-    nav{
-      display: inline-flex;
-      align-items: center;
-      justify-content: space-between;
-      margin-top: 1rem;
-      width: 100%;
-      ul{
-        display: inline-flex;
-        align-items: center;
-        padding: 0;
-        margin: 0;
-      }
-      li{
-        margin: 0 1rem;
-        text-transform: uppercase;
-        cursor: pointer;
-      }
-    }
-    .header{
-      display: flex;
-      flex-direction: column;
-      gap: 2rem;
-      align-items: center;
-      justify-content: center;
-      text-transform: uppercase;
-      padding-bottom: 8rem;
-      img{
-        width: 60%;
-        height: auto;
-      }
-    }
-  }
-</style>
