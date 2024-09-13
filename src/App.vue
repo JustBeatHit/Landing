dcc9075f92787f999935546db38c900ccf83fd4a
YannDecelle <yann.decelle@edu.devinci.fr>
2024-09-13 10:02:46 +0200

    :lipstick: added foooter

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import Header from './components/Header.vue';
 import ServiceContainer from './components/ServiceContainer.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <!-- Import Header -->
   <Header />
   <ServiceContainer />
+  <!-- Import Footer -->
+  <Footer />
 </template>
